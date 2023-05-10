3ee4a4f3f5c7b879c58433642d666e81cb696c1b
Mertha89 <mertha@live.de>
2023-05-10 09:39:17 +0200

    zwischenspeicher

--- script_01.js
+++ script_01.js
@@ -3,3 +3,40 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+
+const ageJohn = 30;
+const ageMark = 30;
+
+// console.log(ageJohn);
+// console.log(ageMark);
+
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// test | Logische Aussage
+
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+
+console.log("ageJohn: ", ageJohn);
+console.log("ageMark: ", ageMark);
+console.log("isJohnOlder: ", isJohnOlder);
+console.log("isJohnEqual: ", isJohnEqual);
+console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)
+if(false)
+{
+    console.log("John ist älter.");
+
+
+}
+
